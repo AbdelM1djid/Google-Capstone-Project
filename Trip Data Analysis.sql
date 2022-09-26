e818756f130daabb115467cb9f534ebd42f245ca
Madjid Erroukrma <104724832+AbdelM1djid@users.noreply.github.com>
2022-09-26 11:20:58 +0100

    Delete Trip Data Analysis.sql

--- Trip Data Analysis.sql
+++
@@ -1,7 +0,0 @@
--- unlike the rest of the data 2022_06 has a changed data type  , lets view the data 
-SELECT * 
-FROM "2022_06" t1 
-
--- the start id column is integer in ("2022_06" Data )  unlike the rest of the data , Lets convert it to VARCHAR 
-ALTER TABLE t1
-ALTER COLUMN 'start_station_id' VARCHAR(50);
